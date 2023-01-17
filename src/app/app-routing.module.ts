3cace82faf97ced7b1f1f35fff8c74238520c73b
Elijah Winterton <elijah@wintertons.us>
2023-01-17 11:16:31 -0500

    add routing and page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './partial/page/page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'nav/:page', component: PageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './partial/app-header/app-header.component';
+import { AppContentComponent } from './partial/app-content/app-content.component';
+import { QuotesComponent } from './partial/quotes/quotes.component';
+import { PageComponent } from './partial/page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppContentComponent,
+    QuotesComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
